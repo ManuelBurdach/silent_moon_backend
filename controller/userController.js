b192f30cc2aaf468a215843c7d2164fc0f2a1af2
ManuelBurdach <103679985+ManuelBurdach@users.noreply.github.com>
2023-05-11 15:03:05 +0200

    new commit

--- controller/userController.js
+++ controller/userController.js
@@ -4,10 +4,12 @@ import { createJWT } from "../util/token.js";
 
 // --------------------------------------------- DB_COLLECTION
 const USER_COL = process.env.USER_COL;
+const isLoggedInObj = "";
+const guestObj = { id: 0, email: "", firstName: "guest", isLoggedIn: false };
 
 // --------------------------------------------- USER_VERIY
 export const first_verify = async (req, res) => {
-  res.json({ id: 0, email: "", firstName: "guest", isLoggedIn: false });
+  res.json({ ...req.claim, isLoggedIn: true });
 };
 // --------------------------------------------- USER_DETAILS
 export const details = async (req, res) => {
@@ -29,9 +31,9 @@ export const login = async (req, res) => {
   try {
     const db = await getDb();
     const result = await db.collection(USER_COL).findOne(req.body);
-    if (!result) res.status(401).json({ id: 0, email: "", firstName: "guest", isLoggedIn: false });
+    if (!result) res.status(401).json(guestObj);
     else {
-      const token = createJWT({ email: result.email });
+      const token = createJWT({ id: result._id, email: result.email, firstName: result.firstName }); //auch in register function ändern
       res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" });
       res.json({
         id: result._id,
@@ -41,7 +43,7 @@ export const login = async (req, res) => {
       });
     }
   } catch (err) {
-    res.status(500).json({ id: 0, email: "", firstName: "guest", isLoggedIn: false });
+    res.status(500).json(guestObj);
   }
 };
 
@@ -63,12 +65,11 @@ export const register = async (req, res) => {
     //check email exists
     const checkEmail = await db.collection(USER_COL).findOne({ email: req.body.email });
     if (checkEmail) {
-      res.status(550).json({ id: 0, email: "", firstName: "guest", isLoggedIn: false }); //550 = Email existiert schon
+      res.status(550).json(guestObj); //550 = Email existiert schon
     } else {
       //user register
       const result = await db.collection(USER_COL).insertOne(user);
-      console.log(result);
-      const token = createJWT({ email: req.body.email });
+      const token = createJWT({ id: result._id, email: result.email, firstName: result.firstName }); // auch in loggin func ändern
       res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" });
       res.json({
         id: result.insertedId,
--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -4,7 +4,7 @@ export const verifyUser = (req, res, next) => {
   try {
     const token = req.cookies.token;
     const result = verifyJWT(token);
-    req.claim = { email: result.email };
+    req.claim = { id: result._id, email: result.email, firstName: result.firstName };
     next();
   } catch (err) {
     res.status(401).json({ id: 0, firstName: "guest", isLoggedIn: false });
